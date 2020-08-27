65156d77af1bdc95b1b61dc259fad9a665445d88
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-08-27 23:16:23 +0200

    add init functions
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -41,29 +41,91 @@ use std::{
 };
 
 static GLOBAL_EXECUTOR_INIT: AtomicBool = AtomicBool::new(false);
+static GLOBAL_EXECUTOR_THREADS: Lazy<()> = Lazy::new(init);
+
 static GLOBAL_EXECUTOR: Executor = Executor::new();
 
 thread_local! {
     static LOCAL_EXECUTOR: LocalExecutor = LocalExecutor::new();
 }
 
-fn init() {
+/// Configuration to init the thread pool for the multi-threaded global executor.
+#[derive(Default, Debug)]
+pub struct GlobalExecutorConfig {
+    /// The environment variable from which we'll try to parse the number of threads to spawn.
+    pub env_var: Option<&'static str>,
+    /// The default number of threads to spawn.
+    pub default_threads: Option<usize>,
+    /// The prefix used to name the threads.
+    pub thread_name_prefix: Option<&'static str>,
+}
+
+impl GlobalExecutorConfig {
+    /// Use the specified environment variable to find the number of threads to spawn.
+    pub fn with_env_var(mut self, env_var: &'static str) -> Self {
+        self.env_var = Some(env_var);
+        self
+    }
+
+    /// Use the specified value as the default number of threads.
+    pub fn with_default_threads(mut self, default_threads: usize) -> Self {
+        self.default_threads = Some(default_threads);
+        self
+    }
+
+    /// Use the specified prefix to name the threads.
+    pub fn with_thread_name_prefix(mut self, env_var: &'static str) -> Self {
+        self.env_var = Some(env_var);
+        self
+    }
+}
+
+/// Init the global executor, spawning as many threads as specified or
+/// the value specified by the specified environment variable.
+///
+/// # Examples
+///
+/// ```
+/// async_global_executor::init_with_config(
+///     async_global_executor::GlobalExecutorConfig::default()
+///         .with_env_var("NUMBER_OF_THREADS")
+///         .with_default_threads(4)
+///         .with_thread_name_prefix("thread-")
+/// );
+/// ```
+pub fn init_with_config(config: GlobalExecutorConfig) {
     if !GLOBAL_EXECUTOR_INIT.compare_and_swap(false, true, Ordering::AcqRel) {
-        let num_cpus = std::env::var("ASYNC_GLOBAL_EXECUTOR_THREADS")
+        let num_cpus = std::env::var(config.env_var.unwrap_or("ASYNC_GLOBAL_EXECUTOR_THREADS"))
             .ok()
             .and_then(|threads| threads.parse().ok())
-            .unwrap_or_else(num_cpus::get)
-            .max(1);
+            .unwrap_or(config.default_threads.unwrap_or_else(num_cpus::get));
         for n in 1..=num_cpus {
             thread::Builder::new()
-                .name(format!("async-global-executor-{}", n))
+                .name(format!(
+                    "{}{}",
+                    config
+                        .thread_name_prefix
+                        .unwrap_or("async-global-executor-"),
+                    n
+                ))
                 .spawn(|| run(future::pending::<()>()))
                 .expect("cannot spawn executor thread");
         }
     }
 }
 
-static GLOBAL_EXECUTOR_THREADS: Lazy<()> = Lazy::new(init);
+/// Init the global executor, spawning as many threads as the number or cpus or
+/// the value specified by the `ASYNC_GLOBAL_EXECUTOR_THREADS` environment variable
+/// if specified.
+///
+/// # Examples
+///
+/// ```
+/// async_global_executor::init();
+/// ```
+pub fn init() {
+    init_with_config(GlobalExecutorConfig::default());
+}
 
 /// Runs the global and the local executor on the current thread
 ///
