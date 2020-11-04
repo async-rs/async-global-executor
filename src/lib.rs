9deed5cdfcd5dc0b94423f206cdccfe9da9a2601
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-11-04 09:43:51 +0100

    rearrange code
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -43,19 +43,7 @@ use std::{
 
 pub use async_executor::Task;
 
-mod reactor {
-    pub(crate) fn block_on<F: std::future::Future<Output = T>, T>(future: F) -> T {
-        #[cfg(feature = "async-io")]
-        let run = || async_io::block_on(future);
-        #[cfg(not(feature = "async-io"))]
-        let run = || future::block_on(future);
-        #[cfg(feature = "tokio02")]
-        let run = || crate::TOKIO02.enter(|| run());
-        #[cfg(feature = "tokio03")]
-        let _tokio03_enter = crate::TOKIO03.enter();
-        run()
-    }
-}
+// Global state
 
 static GLOBAL_EXECUTOR_CONFIG: OnceCell<Config> = OnceCell::new();
 static GLOBAL_EXECUTOR_THREADS: Lazy<()> = Lazy::new(init);
@@ -69,170 +57,7 @@ thread_local! {
     static LOCAL_EXECUTOR: LocalExecutor<'static> = LocalExecutor::new();
 }
 
-#[cfg(feature = "tokio02")]
-static TOKIO02: Lazy<tokio02_crate::runtime::Handle> = Lazy::new(|| {
-    let mut rt = tokio02_crate::runtime::Runtime::new().expect("failed to build tokio02 runtime");
-    let handle = rt.handle().clone();
-    thread::Builder::new()
-        .name("async-global-executor/tokio02".to_string())
-        .spawn(move || {
-            rt.block_on(future::pending::<()>());
-        })
-        .expect("failed to spawn tokio02 driver thread");
-    handle
-});
-
-#[cfg(feature = "tokio03")]
-static TOKIO03: Lazy<tokio03_crate::runtime::Runtime> = Lazy::new(|| {
-    thread::Builder::new()
-        .name("async-global-executor/tokio03".to_string())
-        .spawn(move || {
-            TOKIO03.block_on(future::pending::<()>());
-        })
-        .expect("failed to spawn tokio03 driver thread");
-    tokio03_crate::runtime::Runtime::new().expect("failed to build tokio03 runtime")
-});
-
-/// Configuration to init the thread pool for the multi-threaded global executor.
-#[derive(Default, Debug)]
-pub struct GlobalExecutorConfig {
-    /// The environment variable from which we'll try to parse the number of threads to spawn.
-    pub env_var: Option<&'static str>,
-    /// The default number of threads to spawn.
-    pub default_threads: Option<usize>,
-    /// The name to us fo the threads.
-    pub thread_name: Option<String>,
-    /// The prefix used to name the threads.
-    pub thread_name_prefix: Option<&'static str>,
-}
-
-impl GlobalExecutorConfig {
-    /// Use the specified environment variable to find the number of threads to spawn.
-    pub fn with_env_var(mut self, env_var: &'static str) -> Self {
-        self.env_var = Some(env_var);
-        self
-    }
-
-    /// Use the specified value as the default number of threads.
-    pub fn with_default_threads(mut self, default_threads: usize) -> Self {
-        self.default_threads = Some(default_threads);
-        self
-    }
-
-    /// Use the specified prefix to name the threads.
-    pub fn with_thread_name(mut self, thread_name: String) -> Self {
-        self.thread_name = Some(thread_name);
-        self
-    }
-
-    /// Use the specified prefix to name the threads.
-    pub fn with_thread_name_prefix(mut self, thread_name_prefix: &'static str) -> Self {
-        self.thread_name_prefix = Some(thread_name_prefix);
-        self
-    }
-
-    fn seal(self) -> Config {
-        let num_threads = std::env::var(self.env_var.unwrap_or("ASYNC_GLOBAL_EXECUTOR_THREADS"))
-            .ok()
-            .and_then(|threads| threads.parse().ok())
-            .or(self.default_threads)
-            .unwrap_or_else(num_cpus::get)
-            .max(1);
-        Config {
-            min_threads: num_threads,
-            max_threads: num_threads * 4, // FIXME: make this configurable in 2.0
-            thread_name: self.thread_name, // FIXME: make this a Fn in 2.0
-            thread_name_prefix: self.thread_name_prefix,
-        }
-    }
-}
-
-struct Config {
-    min_threads: usize,
-    max_threads: usize,
-    thread_name: Option<String>,
-    thread_name_prefix: Option<&'static str>,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        GlobalExecutorConfig::default().seal()
-    }
-}
-
-/// Init the global executor, spawning as many threads as specified or
-/// the value specified by the specified environment variable.
-///
-/// # Examples
-///
-/// ```
-/// async_global_executor::init_with_config(
-///     async_global_executor::GlobalExecutorConfig::default()
-///         .with_env_var("NUMBER_OF_THREADS")
-///         .with_default_threads(4)
-///         .with_thread_name_prefix("thread-")
-/// );
-/// ```
-pub fn init_with_config(config: GlobalExecutorConfig) {
-    let _ = GLOBAL_EXECUTOR_CONFIG.set(config.seal());
-    init();
-}
-
-/// Init the global executor, spawning as many threads as the number or cpus or
-/// the value specified by the `ASYNC_GLOBAL_EXECUTOR_THREADS` environment variable
-/// if specified.
-///
-/// # Examples
-///
-/// ```
-/// async_global_executor::init();
-/// ```
-pub fn init() {
-    let config = GLOBAL_EXECUTOR_CONFIG.get_or_init(Config::default);
-    if GLOBAL_EXECUTOR_THREADS_NUMBER.load(Ordering::SeqCst) == 0 {
-        spawn_more_threads(config.min_threads).expect("cannot spawn executor threads");
-    }
-}
-
-/// Spawn more executor threads, up to configured max value.
-///
-/// # Examples
-///
-/// ```
-/// async_global_executor::spawn_more_threads(2);
-/// ```
-pub fn spawn_more_threads(count: usize) -> io::Result<()> {
-    let config = GLOBAL_EXECUTOR_CONFIG.get().unwrap_or_else(|| {
-        Lazy::force(&GLOBAL_EXECUTOR_THREADS);
-        GLOBAL_EXECUTOR_CONFIG.get().unwrap()
-    });
-    let count = count.min(config.max_threads - GLOBAL_EXECUTOR_THREADS_NUMBER.load(Ordering::SeqCst));
-    for _ in 0..count {
-        thread::Builder::new()
-            .name(config.thread_name.clone().unwrap_or_else(|| {
-                format!(
-                    "{}{}",
-                    config
-                        .thread_name_prefix
-                        .unwrap_or("async-global-executor-"),
-                    GLOBAL_EXECUTOR_NEXT_THREAD.fetch_add(1, Ordering::SeqCst),
-                )
-            }))
-            .spawn(|| loop {
-                let _ = std::panic::catch_unwind(|| {
-                    LOCAL_EXECUTOR.with(|executor| {
-                        let local = executor.run(future::pending::<()>());
-                        let global = GLOBAL_EXECUTOR.run(future::pending::<()>());
-                        reactor::block_on(future::or(local, global))
-                    })
-                });
-            })?;
-            if GLOBAL_EXECUTOR_THREADS_NUMBER.fetch_add(1, Ordering::SeqCst) >= config.max_threads {
-                break;
-            }
-    }
-    Ok(())
-}
+// Executor methods
 
 /// Runs the global and the local executor on the current thread
 ///
@@ -302,10 +127,185 @@ pub fn spawn_local<F: Future<Output = T> + 'static, T: 'static>(future: F) -> Ta
     LOCAL_EXECUTOR.with(|executor| executor.spawn(future))
 }
 
-#[cfg(all(test, feature = "tokio02"))]
-mod test_tokio02 {
+// Configuration
+
+/// Configuration to init the thread pool for the multi-threaded global executor.
+#[derive(Default, Debug)]
+pub struct GlobalExecutorConfig {
+    /// The environment variable from which we'll try to parse the number of threads to spawn.
+    pub env_var: Option<&'static str>,
+    /// The default number of threads to spawn.
+    pub default_threads: Option<usize>,
+    /// The name to us fo the threads.
+    pub thread_name: Option<String>,
+    /// The prefix used to name the threads.
+    pub thread_name_prefix: Option<&'static str>,
+}
+
+impl GlobalExecutorConfig {
+    /// Use the specified environment variable to find the number of threads to spawn.
+    pub fn with_env_var(mut self, env_var: &'static str) -> Self {
+        self.env_var = Some(env_var);
+        self
+    }
+
+    /// Use the specified value as the default number of threads.
+    pub fn with_default_threads(mut self, default_threads: usize) -> Self {
+        self.default_threads = Some(default_threads);
+        self
+    }
+
+    /// Use the specified prefix to name the threads.
+    pub fn with_thread_name(mut self, thread_name: String) -> Self {
+        self.thread_name = Some(thread_name);
+        self
+    }
+
+    /// Use the specified prefix to name the threads.
+    pub fn with_thread_name_prefix(mut self, thread_name_prefix: &'static str) -> Self {
+        self.thread_name_prefix = Some(thread_name_prefix);
+        self
+    }
+
+    fn seal(self) -> Config {
+        let num_threads = std::env::var(self.env_var.unwrap_or("ASYNC_GLOBAL_EXECUTOR_THREADS"))
+            .ok()
+            .and_then(|threads| threads.parse().ok())
+            .or(self.default_threads)
+            .unwrap_or_else(num_cpus::get)
+            .max(1);
+        Config {
+            min_threads: num_threads,
+            max_threads: num_threads * 4, // FIXME: make this configurable in 2.0
+            thread_name: self.thread_name, // FIXME: make this a Fn in 2.0
+            thread_name_prefix: self.thread_name_prefix,
+        }
+    }
+}
+
+struct Config {
+    min_threads: usize,
+    max_threads: usize,
+    thread_name: Option<String>,
+    thread_name_prefix: Option<&'static str>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        GlobalExecutorConfig::default().seal()
+    }
+}
+
+// Initialization
+
+/// Init the global executor, spawning as many threads as specified or
+/// the value specified by the specified environment variable.
+///
+/// # Examples
+///
+/// ```
+/// async_global_executor::init_with_config(
+///     async_global_executor::GlobalExecutorConfig::default()
+///         .with_env_var("NUMBER_OF_THREADS")
+///         .with_default_threads(4)
+///         .with_thread_name_prefix("thread-")
+/// );
+/// ```
+pub fn init_with_config(config: GlobalExecutorConfig) {
+    let _ = GLOBAL_EXECUTOR_CONFIG.set(config.seal());
+    init();
+}
+
+/// Init the global executor, spawning as many threads as the number or cpus or
+/// the value specified by the `ASYNC_GLOBAL_EXECUTOR_THREADS` environment variable
+/// if specified.
+///
+/// # Examples
+///
+/// ```
+/// async_global_executor::init();
+/// ```
+pub fn init() {
+    let config = GLOBAL_EXECUTOR_CONFIG.get_or_init(Config::default);
+    if GLOBAL_EXECUTOR_THREADS_NUMBER.load(Ordering::SeqCst) == 0 {
+        spawn_more_threads(config.min_threads).expect("cannot spawn executor threads");
+    }
+}
+
+/// Spawn more executor threads, up to configured max value.
+///
+/// # Examples
+///
+/// ```
+/// async_global_executor::spawn_more_threads(2);
+/// ```
+pub fn spawn_more_threads(count: usize) -> io::Result<()> {
+    let config = GLOBAL_EXECUTOR_CONFIG.get().unwrap_or_else(|| {
+        Lazy::force(&GLOBAL_EXECUTOR_THREADS);
+        GLOBAL_EXECUTOR_CONFIG.get().unwrap()
+    });
+    let count =
+        count.min(config.max_threads - GLOBAL_EXECUTOR_THREADS_NUMBER.load(Ordering::SeqCst));
+    for _ in 0..count {
+        thread::Builder::new()
+            .name(config.thread_name.clone().unwrap_or_else(|| {
+                format!(
+                    "{}{}",
+                    config
+                        .thread_name_prefix
+                        .unwrap_or("async-global-executor-"),
+                    GLOBAL_EXECUTOR_NEXT_THREAD.fetch_add(1, Ordering::SeqCst),
+                )
+            }))
+            .spawn(|| loop {
+                let _ = std::panic::catch_unwind(|| {
+                    LOCAL_EXECUTOR.with(|executor| {
+                        let local = executor.run(future::pending::<()>());
+                        let global = GLOBAL_EXECUTOR.run(future::pending::<()>());
+                        reactor::block_on(future::or(local, global))
+                    })
+                });
+            })?;
+        if GLOBAL_EXECUTOR_THREADS_NUMBER.fetch_add(1, Ordering::SeqCst) >= config.max_threads {
+            break;
+        }
+    }
+    Ok(())
+}
+
+// Internals
+
+mod reactor {
+    pub(crate) fn block_on<F: std::future::Future<Output = T>, T>(future: F) -> T {
+        #[cfg(feature = "async-io")]
+        let run = || async_io::block_on(future);
+        #[cfg(not(feature = "async-io"))]
+        let run = || future::block_on(future);
+        #[cfg(feature = "tokio02")]
+        let run = || crate::TOKIO02.enter(|| run());
+        #[cfg(feature = "tokio03")]
+        let _tokio03_enter = crate::TOKIO03.enter();
+        run()
+    }
+}
+
+// Tokio integration
+
+#[cfg(feature = "tokio03")]
+static TOKIO03: Lazy<tokio03_crate::runtime::Runtime> = Lazy::new(|| {
+    thread::Builder::new()
+        .name("async-global-executor/tokio03".to_string())
+        .spawn(move || {
+            TOKIO03.block_on(future::pending::<()>());
+        })
+        .expect("failed to spawn tokio03 driver thread");
+    tokio03_crate::runtime::Runtime::new().expect("failed to build tokio03 runtime")
+});
+
+#[cfg(all(test, feature = "tokio03"))]
+mod test_tokio03 {
     use super::*;
-    use tokio02_crate as tokio;
+    use tokio03_crate as tokio;
 
     async fn compute() -> u8 {
         tokio::spawn(async { 1 + 2 }).await.unwrap()
@@ -324,10 +324,23 @@ mod test_tokio02 {
     }
 }
 
-#[cfg(all(test, feature = "tokio03"))]
-mod test_tokio03 {
+#[cfg(feature = "tokio02")]
+static TOKIO02: Lazy<tokio02_crate::runtime::Handle> = Lazy::new(|| {
+    let mut rt = tokio02_crate::runtime::Runtime::new().expect("failed to build tokio02 runtime");
+    let handle = rt.handle().clone();
+    thread::Builder::new()
+        .name("async-global-executor/tokio02".to_string())
+        .spawn(move || {
+            rt.block_on(future::pending::<()>());
+        })
+        .expect("failed to spawn tokio02 driver thread");
+    handle
+});
+
+#[cfg(all(test, feature = "tokio02"))]
+mod test_tokio02 {
     use super::*;
-    use tokio03_crate as tokio;
+    use tokio02_crate as tokio;
 
     async fn compute() -> u8 {
         tokio::spawn(async { 1 + 2 }).await.unwrap()
