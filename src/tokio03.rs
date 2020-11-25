e5edf54419c3cfc2c0ef8bf6985291425eeb7452
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-11-25 10:06:58 +0100

    tokio03: don't spawn a Runtime if there's already one
    matching what we already do for tokio02
    
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/tokio03.rs
+++ src/tokio03.rs
@@ -5,14 +5,20 @@ pub(crate) fn enter() -> tokio::runtime::EnterGuard<'static> {
     RUNTIME.enter()
 }
 
-static RUNTIME: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
-    std::thread::Builder::new()
-        .name("async-global-executor/tokio03".to_string())
-        .spawn(move || {
-            RUNTIME.block_on(futures_lite::future::pending::<()>());
-        })
-        .expect("failed to spawn tokio03 driver thread");
-    tokio::runtime::Runtime::new().expect("failed to build tokio03 runtime")
+static RUNTIME: Lazy<tokio::runtime::Handle> = Lazy::new(|| {
+    if let Ok(handle) = tokio::runtime::Handle::try_current() {
+        handle.clone()
+    } else {
+        let rt = tokio::runtime::Runtime::new().expect("failed to build tokio03 runtime");
+        let handle = rt.handle().clone();
+        std::thread::Builder::new()
+            .name("async-global-executor/tokio03".to_string())
+            .spawn(move || {
+                rt.block_on(futures_lite::future::pending::<()>());
+            })
+            .expect("failed to spawn tokio03 driver thread");
+        handle
+    }
 });
 
 #[cfg(test)]
